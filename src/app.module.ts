d1a59290944f2042edbf4f8e0961710d7f1bd46a
Sagarideen <isagarideen@gmail.com>
2023-03-10 10:07:13 +0530

    [Update] Swagger,Docker,Pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entity/user-entity';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user1',
+      password: 'changeme',
+      database: 'ECommerce',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Ecommerce')
+  .setDescription('Nest Js Application')
+  .setVersion('1.0')
+  .addTag('Nest')
+  .addBearerAuth(
+    { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+    'JWT',
+  )
+  .build();
+// const document = SwaggerModule.createDocument(app, config);
+// SwaggerModule.setup('api', app, document);
+app.useGlobalPipes(new ValidationPipe({
+  whitelist: true,
+  transform:true,
+  forbidNonWhitelisted: true,
+  transformOptions: {
+    enableImplicitConversion: true
+  }
+})); 
+
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document, {
+  swaggerOptions: {
+    tagsSorter: 'alpha',
+    operationsSorter: 'alpha',
+    persistAuthorization: true,
+  },
+});
   await app.listen(3000);
 }
 bootstrap();
